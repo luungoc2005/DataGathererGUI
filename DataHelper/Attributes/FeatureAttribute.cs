eb6f890a98d89643f74484f73a13137972dcb59b
DESKTOP-8CL99LH\Gasto <luungoc2005@gmail.com>
2017-01-04 18:01:18 +0700

    test

---
+++ DataHelper/Attributes/FeatureAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataHelper.Attributes
+{
+    public class Feature : Attribute
+    {
+    }
+}
